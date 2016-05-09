199045ae30f849afa5b7042b00a984ba28516bd0
Xin He <Xin.He@ingenta.com>
2016-05-09 10:50:39 +0100

    Second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("I will be living in 17 Redhatch Drive for a while.");
+		System.out.println("I am in Ingenta Oxford office now.");
 	}	
 }
