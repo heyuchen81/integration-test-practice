8a660b420b1bfea84b8f0c835287477f31493867
Xin He <Xin.He@ingenta.com>
2016-05-09 09:36:47 +0100

    First commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I will be living in 17 Redhatch Drive for a while.");
+	}	
+}
